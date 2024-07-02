1b40199444ae19eb200fba14a4a9abc12ca4bff1
Jonny McNamee <jonathanmc.jm94@gmail.com>
2024-07-02 16:22:17 -0700

    Updating error handling

--- server/src/routes/chat.ts
+++ server/src/routes/chat.ts
@@ -4,31 +4,56 @@ import Message from "../models/message";
 const router = express.Router();
 
 router.get("/message/:userID/:id", async (req: Request, res: Response) => {
-    const userId = Number(req.params.userID);
-    const messageId = Number(req.params.id);
+  const userId = Number(req.params.userID);
+  const messageId = Number(req.params.id);
+
+  try {
     const message = await Message.findById(userId, messageId);
     res.json(message);
+  } catch (err: any) {
+    res.status(404).json({ message: err.message });
+  }
 });
 
 router.get("/messages/:userID", async (req: Request, res: Response) => {
-    const userId = Number(req.params.userID);
+  const userId = Number(req.params.userID);
+
+  try {
     const messages = await Message.findByUserId(userId);
     res.json(messages);
+  } catch (err: any) {
+    res.status(404).json({ message: err.message });
+  }
 });
 
 router.post("/message", async (req: Request, res: Response) => {
+  try {
     const newMessage = await Message.create(req.body.user_id, req.body.message);
     res.json(newMessage);
+  } catch (err: any) {
+    res.status(404).json({ message: err.message });
+  }
 });
 
 router.put("/message/:id", async (req: Request, res: Response) => {
-    const updatedMessage = await Message.update(Number(req.params.id), req.body.message);
+  try {
+    const updatedMessage = await Message.update(
+      Number(req.params.id),
+      req.body.message
+    );
     res.json(updatedMessage);
+  } catch (err: any) {
+    res.status(404).json({ message: err.message });
+  }
 });
 
 router.delete("/message/:id", async (req: Request, res: Response) => {
+  try {
     const deletedMessage = await Message.delete(Number(req.params.id));
     res.json(deletedMessage);
+  } catch (err: any) {
+    res.status(404).json({ message: err.message });
+  }
 });
 
 export default router;
