915c22c2f050292e25146a3d24822cbedaa292c4
Jonny McNamee <jonathanmc.jm94@gmail.com>
2024-07-03 15:40:20 -0700

    Some more changes to work on authentication

---
+++ client/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "chatful4f1a0c96": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
