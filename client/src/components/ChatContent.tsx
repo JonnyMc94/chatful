9839bd278ac16f8cb5b0e7f4d5dec820def46e61
Jonny McNamee <jonathanmc.jm94@gmail.com>
2025-03-22 17:51:32 -0700

    Scroll to the bottom of chat when its loaded & also consolidating useEffects

--- client/src/components/ChatContent.tsx
+++ client/src/components/ChatContent.tsx
@@ -1,75 +1,89 @@
-import { Message, User } from '../common/types';
+import React, { useEffect, useState, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../state/store";
+import {
+  setMessages,
+  addMessage,
+  updateMessage,
+  deleteMessage,
+} from "../state/chatSlice";
+import { setSelectedUser } from "../state/userSlice";
+import axios from "axios";
+import io from "socket.io-client";
 import MessageHolder from "./MessageHolder";
 import MessageBox from "./MessageBox";
-import UserSelectionModal from '../modals/UserSelectionModal';
+import UserSelectionModal from "../modals/UserSelectionModal";
 import { decodeJWT } from "../utils/decodeJWT";
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setMessages, addMessage, updateMessage, deleteMessage } from '../state/chatSlice';
-import { setSelectedUser } from '../state/userSlice';
-import { RootState } from '../state/store';
-import io from 'socket.io-client';
-import axios from "axios";
+import { Message, User } from "../common/types";
 
-const socket = io('http://localhost:3000'); 
+const socket = io("http://localhost:3000");
 
 const ChatContent = () => {
   const messages = useSelector((state: RootState) => state.chat.messages);
-  const activeChatId = useSelector((state: RootState) => state.chat.activeChatId);
-  const [users, setUsers] = useState<User[]>([]);
-  const [displayUserModal, setDisplayUserModal] = useState<boolean>(false);
-  const selectedUser = useSelector((state: RootState) => state.users.selectedUser);
-
+  const activeChatId = useSelector(
+    (state: RootState) => state.chat.activeChatId
+  );
+  const selectedUser = useSelector(
+    (state: RootState) => state.users.selectedUser
+  );
   const dispatch = useDispatch();
   const currentUserId = decodeJWT()?.userId;
-  
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+  const [users, setUsers] = useState<User[]>([]);
+  const [displayUserModal, setDisplayUserModal] = useState<boolean>(false);
+
+  // Scroll to the bottom of the chat
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  //useEffect for fetching data and socket listeners
   useEffect(() => {
-    // Fetch users from the server
-    const fetchUsers = async () => {
+    const fetchInitialData = async () => {
       try {
-        const response = await axios.get('http://localhost:3000/user/users');
-        setUsers(response.data);
+        // Fetch users
+        const usersResponse = await axios.get("http://localhost:3000/user/users");
+        setUsers(usersResponse.data);
+
+        // Fetch messages for the active chat
+        if (activeChatId) {
+          const messagesResponse = await axios.get(
+            `http://localhost:3000/chat/conversation/${activeChatId}`
+          );
+          dispatch(setMessages(messagesResponse.data));
+        }
       } catch (error) {
-        console.error("Error fetching users:", error);
+        console.error("Error fetching initial data:", error);
       }
     };
 
-    fetchUsers();
+    fetchInitialData();
 
-    socket.on('newMessage', (message: Message) => {
+    // Socket listeners for real-time updates
+    socket.on("newMessage", (message: Message) => {
       dispatch(addMessage(message));
     });
 
-    socket.on('updateMessage', (updatedMessage) => {
+    socket.on("updateMessage", (updatedMessage) => {
       dispatch(updateMessage(updatedMessage));
     });
 
-    socket.on('deleteMessage', (deletedMessage) => {
+    socket.on("deleteMessage", (deletedMessage) => {
       dispatch(deleteMessage(deletedMessage));
     });
 
+    // Cleanup socket listeners on unmount
     return () => {
-      socket.off('newMessage');
-      socket.off('updateMessage');
-      socket.off('deleteMessage');
+      socket.off("newMessage");
+      socket.off("updateMessage");
+      socket.off("deleteMessage");
     };
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (activeChatId) {
-      // Fetch messages for the active chat
-      const fetchMessages = async () => {
-        try {
-          const response = await axios.get(`http://localhost:3000/chat/conversation/${activeChatId}`);
-          dispatch(setMessages(response.data));
-        } catch (error) {
-          console.error("Error fetching messages:", error);
-        }
-      };
-
-      fetchMessages();
-    }
   }, [activeChatId, dispatch]);
+
+  // Scroll to bottom when messages or activeChatId change
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages, activeChatId]);
 
   const handleSendMessage = async (message: string) => {
     if (!activeChatId) {
@@ -78,14 +92,16 @@ const ChatContent = () => {
     }
 
     try {
-      console.log("Sending message:", message);
-      const response = await axios.post('http://localhost:3000/chat/message', {
+      const response = await axios.post("http://localhost:3000/chat/message", {
         senderId: currentUserId,
         recipientId: selectedUser?.id,
         message,
         conversationId: activeChatId,
       });
       console.log("Message sent:", response.data);
+
+      // Emit the message to the server
+      socket.emit("newMessage", response.data);
     } catch (error) {
       console.error("Error sending message:", error);
     }
@@ -93,19 +109,15 @@ const ChatContent = () => {
 
   return (
     <div className="flex flex-col h-full overflow-hidden">
-      {/* Message List */}
       <div className="flex-grow overflow-y-auto p-6">
         {messages.map((data, index) => (
           <MessageHolder key={index} message={data} />
         ))}
+        <div ref={messagesEndRef}></div>
       </div>
-  
-      {/* MessageBox */}
       <div className="sticky bottom-10 bg-white p-4">
         <MessageBox onSendMessage={handleSendMessage} />
       </div>
-  
-      {/* User Selection Modal */}
       {displayUserModal && (
         <UserSelectionModal
           users={users}
